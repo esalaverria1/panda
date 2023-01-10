cf1adfbe804c2b6326afab3bc5c00a28ba5cd990
Eduardo S <esalaverria@Eduardos-MacBook-Pro.local>
2023-01-10 17:53:53 -0600

    attempt 1

--- board/usb_comms.h
+++ board/usb_comms.h
@@ -39,6 +39,8 @@ int get_health_pkt(void *dat) {
 
   health->interrupt_load = interrupt_load;
 
+  health->fan_power = fan_state.power;
+
   return sizeof(*health);
 }
 
@@ -133,12 +135,12 @@ int usb_cb_control_msg(USB_Setup_TypeDef *setup, uint8_t *resp) {
       break;
     // **** 0xb1: set fan power
     case 0xb1:
-      current_board->set_fan_power(setup->b.wValue.w);
+      fan_set_power(setup->b.wValue.w);
       break;
     // **** 0xb2: get fan rpm
     case 0xb2:
-      resp[0] = (fan_rpm & 0x00FFU);
-      resp[1] = ((fan_rpm & 0xFF00U) >> 8U);
+      resp[0] = (fan_state.rpm & 0x00FFU);
+      resp[1] = ((fan_state.rpm & 0xFF00U) >> 8U);
       resp_len = 2;
       break;
     // **** 0xb3: set phone power
